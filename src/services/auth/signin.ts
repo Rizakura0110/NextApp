49876d6d319f5a6e9fe95651c4749e02286e10d0
Rizakura0110 <rasakura0110@gmail.com>
2022-09-07 08:36:17 +0900

    6-2

--- src/services/auth/signin.ts
+++ src/services/auth/signin.ts
@@ -2,8 +2,8 @@ import { ApiContext, User } from 'types'
 import { fetcher } from 'utils'
 
 export type SigninParams = {
-    username: string
-    password: string
+  username: string
+  password: string
 }
 
 /**
@@ -13,21 +13,20 @@ export type SigninParams = {
  * @returns ログインユーザー
  */
 const signin = async (
-    context: ApiContext,
-    params: SigninParams,
-  ): Promise<User> => {
-    return await fetcher(
-      `${context.apiRootUrl.replace(/\/$/g, '')}/auth/signin`,
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(params),
+  context: ApiContext,
+  params: SigninParams,
+): Promise<User> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, '')}/auth/signin`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
-    )
+      body: JSON.stringify(params),
+    },
+  )
 }
-  
 
 export default signin
